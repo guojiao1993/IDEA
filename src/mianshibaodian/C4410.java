6a7d53a559e185ef62888cc037103a02a9e09e5e
guojiao1993 <wang2012jida@163.com>
2018-06-24 21:04:44 +0800

    Java面试宝典4.4.10中找出字符串中的中文

---
+++ src/mianshibaodian/C4410.java
@@ -0,0 +1,33 @@
+package mianshibaodian;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class C4410 {
+    public static void main(String[] args) {
+        judgeChineseCharactor("Hello World");
+        judgeChineseCharactor("Hello 你 World 好 Tiger 吗 ？");
+    }
+
+    /**
+     * 找出所给字符串中的所有中文，以空格分隔
+     * @param str 中英混合字符串
+     */
+    public static void judgeChineseCharactor(String str) {
+        if(str == null || str.length() == 0) {
+            System.out.println("无汉字");
+            return;
+        }
+        // 判断是否存在中文字符
+        if(str.getBytes().length == str.length()) {
+            System.out.println("无汉字");
+        } else {
+            String regEx = "[\u4e00-\u9fa5]";
+            Pattern pattern = Pattern.compile(regEx);
+            Matcher matcher = pattern.matcher(str);
+            while (matcher.find()) {
+                System.out.print(matcher.group(0) + " ");
+            }
+        }
+    }
+}
