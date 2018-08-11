2ec5c8b6bdc006f694f5afc3ddb768fd669dc52f
guojiao1993 <wang2012jida@163.com>
2018-08-11 12:22:04 +0800

    学习HashCode

---
+++ src/test/HashCodeTest.java
@@ -0,0 +1,9 @@
+package test;
+
+public class HashCodeTest {
+    public static void main(String[] args) {
+        System.out.println("".hashCode());
+        System.out.println("a".hashCode());
+        System.out.println("ab".hashCode());
+    }
+}
