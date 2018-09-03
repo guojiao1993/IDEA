d038d5b1251024a91bf8a34b959092258811e6f4
guojiao1993 <wang2012jida@163.com>
2018-09-03 16:44:59 +0800

    研究String的contains()方法

---
+++ src/String/ContainsTest.java
@@ -0,0 +1,9 @@
+package String;
+
+public class ContainsTest {
+    public static void main(String[] args) {
+        String source = "abcdefghi";
+        String target = "efgi";
+        System.out.println(source.contains(target));
+    }
+}
